434dd515bf27a30ec43ac468507c6f7cc5a725ca
Guilherme <gui.oliveira1992@gmail.com>
2018-07-04 18:10:51 -0300

    Inserido as anotation @Entity e @Table na classe Lancamento.

--- src/main/java/com/gms/pontoInteligente/api/entities/Lancamento.java
+++ src/main/java/com/gms/pontoInteligente/api/entities/Lancamento.java
@@ -2,6 +2,11 @@ package com.gms.pontoInteligente.api.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "lancamento")
 public class Lancamento implements Serializable {
 
 	private static final long serialVersionUID = -801911507014418513L;
